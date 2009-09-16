8f624ea5c77b00eeb0d07c20a17ec1aeb0a22948
Jim Weirich <jim.weirich@gmail.com>
2009-09-16 08:29:35 -0400

    Small refactoring in testing.

--- scheme/testing.scm
+++ scheme/testing.scm
@@ -9,6 +9,15 @@
       (write actual)
       (write ">"))))
 
+(define (not-expected-message expected actual)
+  (with-output-to-string
+    (lambda ()
+      (write "assertion failure: Expected: <")
+      (write expected)
+      (write "> to not equal <")
+      (write actual)
+      (write ">"))))
+
 (define (delta-message expected actual delta)
   (with-output-to-string
     (lambda ()
@@ -22,18 +31,44 @@
       (write (abs (- expected actual)))
       (write ">"))))
 
+(define (not-delta-message expected actual delta)
+  (with-output-to-string
+    (lambda ()
+      (write "assert failure: expected the delta of <")
+      (write expected)
+      (write "> and <")
+      (write actual)
+      (write "> to be greater than <")
+      (write delta)
+      (write "> but was <")
+      (write (abs (- expected actual)))
+      (write ">"))))
+
+(define (assert-with procedure message)
+  (if (not (procedure))
+      (error message)
+      'test-ok))
+
 (define (assert-equal expected actual)
-  (if (not (equal? expected actual))
-      (error (expected-message expected actual))
-      #t) )
+  (assert-with (lambda () (equal? expected actual))
+               (expected-message expected actual)) )
+
+(define (assert-not-equal expected actual)
+  (assert-with (lambda () (not (equal? expected actual)))
+               (not-expected-message expected actual)) )
 
 (define (assert-eq expected actual)
-  (if (not (eq? expected actual))
-      (error (expected-message expected actual))
-      #t))
+  (assert-with (lambda () (eq? expected actual))
+               (expected-message expected actual)))
+
+(define (assert-not-eq expected actual)
+  (assert-with (lambda () (not (eq? expected actual)))
+               (not-expected-message expected actual)))
 
 (define (assert-in-delta expected actual delta)
-  (if (not (< (abs (- actual expected))
-              delta))
-      (error (delta-message expected actual delta))
-      #t))
+  (assert-with (lambda () (< (abs (- actual expected)) delta))
+               (error (delta-message expected actual delta))))
+
+(define (assert-not-in-delta expected actual delta)
+  (assert-with (lambda () (>= (abs (- actual expected)) delta))
+               (error (not-delta-message expected actual delta))))
