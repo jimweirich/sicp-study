87b590c85b06c1051328dce3018201306f790529
Jim Weirich <jim.weirich@gmail.com>
2009-09-17 14:32:55 -0400

    Added comments on where to find a good description of the Fibonacci proof.

---
+++ scheme/week02/ex1.13.scm
@@ -0,0 +1,9 @@
+;; Exercise 1.13:
+;;
+;; Prove that Fib(n) is the closest integer to (phi**n)/sqrt(5), where
+;; phi= (1 + sqrt(5))/2. Hint: Let psi = (1 - sqrt(5))/2. Use
+;; induction and the definition of the Fibonacci numbers (see section
+;; 1.2.2) to prove that Fib(n) = (phi**n - psi**n)/sqrt(5).
+
+;; A good explaination of this proof can be found at:
+;; http://wiki.drewhess.com/wiki/SICP_exercise_1.13
