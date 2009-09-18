eca6d394ee9af46d59ac00aafb79adc79c62e827
Jim Weirich <jim.weirich@gmail.com>
2009-09-18 01:25:51 -0400

    Added exercise 1.14.

--- scheme/week02/book_text.scm
+++ scheme/week02/book_text.scm
@@ -2,6 +2,7 @@
 
 (define (count-change amount)
   (cc amount 5))
+
 (define (cc amount kinds-of-coins)
   (cond ((= amount 0) 1)
         ((or (< amount 0) (= kinds-of-coins 0)) 0)
@@ -10,11 +11,10 @@
                  (cc (- amount
                         (first-denomination kinds-of-coins))
                      kinds-of-coins)))))
+
 (define (first-denomination kinds-of-coins)
   (cond ((= kinds-of-coins 1) 1)
         ((= kinds-of-coins 2) 5)
         ((= kinds-of-coins 3) 10)
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
-
-
