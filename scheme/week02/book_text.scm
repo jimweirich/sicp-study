cdcf32ece50bc93bf40f5fbd7199d9a79c8f9b57
Jim Weirich <jim.weirich@gmail.com>
2009-09-19 23:10:16 -0400

    added prime number testing code to the book text.

--- scheme/week02/book_text.scm
+++ scheme/week02/book_text.scm
@@ -18,3 +18,19 @@
         ((= kinds-of-coins 3) 10)
         ((= kinds-of-coins 4) 25)
         ((= kinds-of-coins 5) 50)))
+
+;; 1.2.6 Example: Testing for Primality
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
