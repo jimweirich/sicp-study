67eb1be74ae42f9a4707667fe1ea01922679f4f2
Jim Weirich <jim.weirich@gmail.com>
2009-09-19 21:02:19 -0400

    refactored 1.16

--- scheme/week02/ex1_16.scm
+++ scheme/week02/ex1_16.scm
@@ -14,21 +14,19 @@
 
 ;; ANSWER ------------------------------------------------------------
 
-(define (linear-exp b n)
-  (cond ((zero? n) 1)
-        (else (* b (linear-exp b (- n 1))))))
+(define (square n) (* n n))
 
+;; This is the original recursive version defined in the book.
 (define (fast-exp b n)
   (cond ((= n 0) 1)
         ((even? n) (square (fast-expt b (/ n 2))))
         (else (* b (fast-expt b (- n 1))))))
 
-(define (square n) (* n n))
-
-(define (fexp b n)
+;; 
+(define (iterative-fast-exp b n)
   (define (loop b n a)
     (cond ((zero? n) a)
-          ((even? n) (fexp-loop (square b) (/ n 2) a))
-          (else (fexp-loop b (- n 1) (* b a)))))
+          ((even? n) (loop (square b) (/ n 2) a))
+          (else (loop b (- n 1) (* b a)))))
   (loop b n 1))
 
--- scheme/week02/ex1_16_test.scm
+++ scheme/week02/ex1_16_test.scm
@@ -1,4 +1,4 @@
 (test-case "Fast Exp with an interative process"
-            (assert-equal 1 (fexp 3 0))
-            (assert-equal 9 (fexp 3 2))
-            (assert-equal 3 (fexp 3 1)))
+            (assert-equal 1 (iterative-fast-exp 3 0))
+            (assert-equal 9 (iterative-fast-exp 3 2))
+            (assert-equal 3 (iterative-fast-exp 3 1)))
