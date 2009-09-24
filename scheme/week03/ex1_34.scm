2c2f9017be7db3a0bd08ba71aa7cc7882af3ac11
Jim Weirich <jim.weirich@gmail.com>
2009-09-24 00:30:10 -0400

    Added 1.34.

---
+++ scheme/week03/ex1_34.scm
@@ -0,0 +1,28 @@
+;; SICP 1.34
+
+;; Exercise 1.34.  Suppose we define the procedure
+
+;; (define (f g)
+;;   (g 2))
+
+;; Then we have
+
+;; (f square)
+;; 4
+
+;; (f (lambda (z) (* z (+ z 1))))
+;; 6
+
+;; What happens if we (perversely) ask the interpreter to evaluate the
+;; combination (f f)? Explain.
+
+;; ANSWER ------------------------------------------------------------
+
+(define (f g) (g 2))
+
+;; Lets expand
+
+;; (f f)
+;; (f 2)
+;; (2 2)
+;; Error: 2 is not a function!
