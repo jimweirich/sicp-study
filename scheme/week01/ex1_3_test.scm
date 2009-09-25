fab0c9f2270f64bb42cb536cda1b92b97afccbcf
Jim Weirich <jim.weirich@gmail.com>
2009-09-25 12:49:50 -0400

    Corrected typo in test name.

--- scheme/week01/ex1_3_test.scm
+++ scheme/week01/ex1_3_test.scm
@@ -9,7 +9,7 @@
 (test-case "Ex 1.3 sum-of-squares"
            (assert-equal 25 (sum-of-squares 3 4)))
 
-(test-case "Ex 13 sum-of-two-largest-squares" 
+(test-case "Ex 1.3 sum-of-two-largest-squares" 
            (assert-equal 41 (sum-of-two-largest-squares 3 4 5))
            (assert-equal 41 (sum-of-two-largest-squares 4 3 5))
            (assert-equal 41 (sum-of-two-largest-squares 4 5 3)))
