bed1ce0e5a3898b1370e9a01e4cb73a73980d433
Jim Weirich <jim.weirich@gmail.com>
2009-10-12 13:08:27 -0400

    Updated solution to 1.12 to not use car/cdr/cons/etc.

--- scheme/week02/ex1_12.scm
+++ scheme/week02/ex1_12.scm
@@ -3,11 +3,24 @@
 ;; Exercise 1.12 The following pattern of numbers is called Pascal's triangle.
 ;;
 ;; The numbers at the edge of the triangle are all 1, and each number
-;; inside the triangle is the sum of the two numbers above it.35 Write
+;; inside the triangle is the sum of the two numbers above it. Write
 ;; a procedure that computes elements of Pascal's triangle by means of
 ;; a recursive process.
 
 ;; ANSWER ------------------------------------------------------------
+
+(define (pascal-element row col)
+  (define (edge? row col)
+    (or (= row 1) (= col 1) (= row col)))
+  (cond ((edge? row col) 1)
+        (else (+ (pascal-element (- row 1) (- col 1))
+                 (pascal-element (- row 1) col)))))
+
+;; -------------------------------------------------------------------
+;; ANOTHER ANSWER ...
+;; That is way over complicated because I didn't read the instructions
+;; carefully.
+;; -------------------------------------------------------------------
 
 ;; Make the next row in the triangle from the current row.
 (define (make-row row)
--- scheme/week02/ex1_12_test.scm
+++ scheme/week02/ex1_12_test.scm
@@ -1,5 +1,17 @@
 ;; SICP 1.12 tests
 
+(test-case "Ex 1.12 Pascal Elements"
+           (assert-equal 1 (pascal-element 1 1))
+           (assert-equal 1 (pascal-element 2 1))
+           (assert-equal 1 (pascal-element 2 2))
+           (assert-equal 1 (pascal-element 3 1))
+           (assert-equal 2 (pascal-element 3 2))
+           (assert-equal 1 (pascal-element 3 3))
+           (assert-equal 1 (pascal-element 4 1))
+           (assert-equal 3 (pascal-element 4 2))
+           (assert-equal 3 (pascal-element 4 3))
+           (assert-equal 1 (pascal-element 4 4)))
+
 (test-case "Ex 1.12 Making Rows"
            (assert-equal '(1) (make-row ()))
            (assert-equal '(1 1) (make-row '(1)))
