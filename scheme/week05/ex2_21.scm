ac02731651b10e41cf775db14700faabc5ba08a4
Jim Weirich <jim.weirich@gmail.com>
2009-10-26 11:22:22 -0400

    Added 2.21

---
+++ scheme/week05/ex2_21.scm
@@ -0,0 +1,27 @@
+;; SICP 2.21
+
+;; Exercise 2.21.  The procedure square-list takes a list of numbers
+;; as argument and returns a list of the squares of those numbers.
+;;
+;; (square-list (list 1 2 3 4))
+;; (1 4 9 16)
+;;
+;; Here are two different definitions of square-list. Complete both of
+;; them by filling in the missing expressions:
+;;
+;; (define (square-list items)
+;;   (if (null? items)
+;;       nil
+;;       (cons <??> <??>)))
+;; (define (square-list items)
+;;   (map <??> <??>))
+
+;; ANSWER ------------------------------------------------------------
+
+(define (square-list-1 items)
+  (if (null? items)
+      nil
+      (cons (square (car items)) (cdr items))))
+
+(define (square-list-2 items)
+  (map (lambda (x) (square x)) items))
---
+++ scheme/week05/ex2_21_test.scm
@@ -0,0 +1,7 @@
+;; SICP Tests 2.21
+
+(test-case "Ex 2.21 Square List 1"
+           (assert-equal (list 1 4 9 16) (square-list-1 (list 1 2 3 4))))
+
+(test-case "Ex 2.21 Square List 1"
+           (assert-equal (list 1 4 9 16) (square-list-2 (list 1 2 3 4))))
